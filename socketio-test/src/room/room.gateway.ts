dc23053abcc77bc359a642a80ba958ab373463be
Ljaewon-123 <jml6388@naver.com>
2024-08-30 18:49:19 +0900

    add 주석

--- socketio-test/src/room/room.gateway.ts
+++ socketio-test/src/room/room.gateway.ts
@@ -6,7 +6,10 @@ import {
 import { Server } from 'socket.io';
 
 // 똑같이 하난데 정보가 다른걸로 주냐 다른애들끼리 다른 room을 배정하냐 
-// room이 좀더 복잡할거같으니까 room으로 할까 
+// room 이 필요가 없을수도있음 다른룸에 접속해서 room정보를 주는거나
+// 그냥 1차적으로 연결해서 payload에 보내는거랑 차이가 없음 
+// room은 복잡하기만 할거같은데 
+// # #// room이 좀더 복잡할거같으니까 room으로 할까 X
 @WebSocketGateway({ namespace: 'socket' })
 export class RoomGateway {
   @WebSocketServer() server: Server;
