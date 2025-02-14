d7dcfd6fd89fb0838192b55b1e368c90b8a20789
Ljaewon-123 <jml6388@naver.com>
2025-02-15 03:28:03 +0900

    add test

--- jest-lecture/src/module.test.ts
+++ jest-lecture/src/module.test.ts
@@ -1,5 +1,10 @@
 import { obj } from "./module"
 
+// 모듈 리셋
+beforeEach(() => {
+  jest.resetModules()
+})
+
 jest.mock('./module')
 test('all module mocking', () => {
   console.log(obj)  // 전부 모킹됨 
