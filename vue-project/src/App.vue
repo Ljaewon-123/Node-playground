b594c4400bbb4548c233e0f13b1604babb94b96e
Ljaewon-123 <jml6388@naver.com>
2025-04-28 17:42:08 +0900

    projects

---
+++ vue-project/src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref, onUpdated } from 'vue'
+import PropsTest from '@/components/icons/PropsTest.vue'
+
+const count = ref(0)
+
+onUpdated(() => {
+  // 텍스트 내용은 현재 `count.value`와 같아야 함
+  console.log(document.getElementById('count').textContent)
+})
+
+const updateCount = () => {
+  count.value = 0
+}
+</script>
+
+<template>
+  <div>
+    <button id="count" @click="updateCount">{{ count }}</button>
+
+    <PropsTest
+      :message="`Count is ${count}`"
+      :cnt="count"
+    />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ vue-project/src/components/icons/PropsTest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ message }}
+    <span>{{ cnt }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onUpdated } from 'vue'
+
+interface Props {
+  message: string
+  cnt: number
+}
+
+const { message = '', cnt = 0 } = defineProps< Props >()
+
+onUpdated(() => {
+  // 텍스트 내용은 현재 `message`와 같아야 함
+  console.log('renderer')
+})
+</script>
