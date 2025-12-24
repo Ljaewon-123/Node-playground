ffb2d05320002605fe5e2d93cf291bb0baf8a25d
Ljaewon-123 <jml6388@naver.com>
2025-12-24 22:18:17 +0900

    nest env setup

---
+++ nest-setup/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { validate } from './config/env.validation';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+      cache: true,
+      validate,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
